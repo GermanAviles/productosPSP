5707e0db1d80a5a27326df08be9dcbaee55cfade
INTEREDES-043 <interedes-043@interedes-043.interedes.local>
2019-11-08 17:18:04 -0500

    pagina para listado de productos

---
+++ lib/src/pages/productos_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ProductoPage extends StatelessWidget {
+
+  const ProductoPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
